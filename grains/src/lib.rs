df4b978622561fe744e47ef7c12922a58e15316c
Dennis O'Harlem <dennis.oharlem@gmail.com>
2020-09-16 13:50:56 -0400

    grains - refs

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,14 +1,10 @@
 pub fn square(s: u32) -> u64 {
     match s {
-        1 => 1 as u64,
-        2..=64 => {
-            let sum: u64 = 2;
-            sum.pow(s - 1)
-        }
+        1..=64 => 1 << (s - 1),
         _ => panic!("Square must be between 1 and 64"),
     }
 }
 
 pub fn total() -> u64 {
-    (square(64) - 1) * 2 + 1
+    (1..=64).map(square).sum()
 }
