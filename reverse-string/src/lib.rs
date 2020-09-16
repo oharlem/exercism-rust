ab0684f141a9fcc75c22229c6527e64c051b90fd
Dennis O'Harlem <dennis.oharlem@gmail.com>
2020-09-16 19:57:47 -0400

    removed into_iter

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -3,7 +3,6 @@ use unicode_segmentation::UnicodeSegmentation;
 pub fn reverse(input: &str) -> String {
     input
         .graphemes(true)
-        .into_iter()
         .rev()
         .collect()
 }
