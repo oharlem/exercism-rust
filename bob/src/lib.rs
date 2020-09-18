9204acce11fb0a722ed8b1e00efa02d9a944da5d
Dennis O'Harlem <dennis.oharlem@gmail.com>
2020-09-18 12:36:28 -0400

    bob refactored to use match

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,28 +1,28 @@
 pub fn reply(message: &str) -> &str {
+    /*
+    conditions:
+    1) question, contains letters, all capital, not empty
+       "Calm down, I know what I'm doing!"
+    2) question, any symbols, any caps, n/a
+        "Sure."
+    3) not a question, contains letters, all capital, not empty
+       "Whoa, chill out!"
+    4) everything else
+       "Whatever."
+
+        + need to trim whitespaces
+     */
     let m = message.trim_end().replace('\r', "");
-    let all_cap = m == m.to_uppercase();
-    let any_alpha = m.chars().any(char::is_alphabetic);
     let is_question = m.ends_with("?");
+    let has_letters = m.chars().any(char::is_alphabetic);
+    let all_capital = m == m.to_uppercase();
+    let is_empty = m.is_empty();
 
-    if is_question {
-        if any_alpha && all_cap {
-            // yelling a question
-            return "Calm down, I know what I'm doing!";
-        }
-
-        // all other questions, inc. input w/o letters
-        return "Sure.";
+    match (is_question, has_letters, all_capital, is_empty) {
+        (true, true, true, false) => "Calm down, I know what I'm doing!",
+        (true, _, _, _) => "Sure.",
+        (false, true, true, false) => "Whoa, chill out!",
+        (_, _, _, true) => "Fine. Be that way!",
+        (_, _, _, _) => "Whatever.",
     }
-
-    // yelling, not a question
-    if any_alpha && all_cap && !is_question {
-        return "Whoa, chill out!";
-    }
-
-    // addressing w/o saying anything (empty string)
-    if m.is_empty() {
-        return "Fine. Be that way!";
-    }
-
-    "Whatever."
 }
--- bob/src/main.rs
+++
@@ -1,7 +0,0 @@
-use bob::reply;
-
-fn main() {
-    let m = reply("\r\r ");
-
-    println!("{}", m);
-}
