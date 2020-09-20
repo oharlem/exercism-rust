9e116ce11a66ef6dbd90bd0fef4009f32747663a
Dennis O'Harlem <dennis.oharlem@gmail.com>
2020-09-19 20:34:47 -0400

    high-scores 1st working draft

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -2,12 +2,13 @@ use std::fmt;
 
 #[derive(Debug)]
 pub struct Clock {
+    hours: i32,
     minutes: i32,
 }
 
 impl PartialEq for Clock {
     fn eq(&self, other: &Self) -> bool {
-        self.minutes == other.minutes
+        self.hours == other.hours && self.minutes == other.minutes
     }
 }
 
@@ -15,26 +16,66 @@ impl Eq for Clock {}
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // calculate hours
-        // calculate minutes
-        // show with padding
-        let mut h = &self.minutes / 60;
-        h = h % 24;
-        let m = &self.minutes % 60;
-        write!(f, "{:02}:{:02}", h, m)
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
+        let mut m = minutes % 60;
+        if m < 0 {
+            m = 60 + m;
+        }
+
+        // adjust hours for rolling over minutes
+        let h_rollover = minutes / 60;
+        let mut h = hours + h_rollover;
+        if minutes < 0 && minutes % 60 != 0 {
+            h -= 1;
+        }
+
+        h %= 24;
+        if h < 0 {
+            h = 24 + h;
+        }
+
         Self {
-            minutes: hours * 60 + minutes,
+            hours: h,
+            minutes: m,
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
+        // recalculate new time
+        /*
+            let clock = Clock::new(10, 3).add_minutes(-3);
+            assert_eq!(clock.to_string(), "10:00");
+
+            let clock = Clock::new(23, 59).add_minutes(2);
+            assert_eq!(clock.to_string(), "00:01");
+         */
+
+        let mut m = &self.minutes + minutes;
+        m %= 60;
+        if m < 0 {
+            m = 60 + m;
+        }
+
+        // adjust hours for rolling over minutes
+        let h_rollover = (&self.minutes + minutes) / 60;
+        let mut h = self.hours + h_rollover;
+        if (&self.minutes + minutes) < 0 && (&self.minutes + minutes) % 60 != 0 {
+            h -= 1;
+        }
+
+        h %= 24;
+        if h < 0 {
+            h = 24 + h;
+        }
+
         Self {
-            minutes: &self.minutes + minutes,
+            hours: h,
+            minutes: m,
         }
     }
 }
