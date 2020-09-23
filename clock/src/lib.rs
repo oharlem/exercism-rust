6c76af8792e690b4b893476da1cf648b235ea100
Dennis O'Harlem <dennis.oharlem@gmail.com>
2020-09-22 23:00:30 -0400

    clock, mor ref

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -16,33 +16,22 @@ impl fmt::Display for Clock {
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let m = Clock::recalculate_time(hours * 60 + minutes);
+        let m = Clock::recalc_time(hours * 60 + minutes);
         Self { minutes: m }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let m = Clock::recalculate_time(self.minutes + minutes);
+        let m = Clock::recalc_time(self.minutes + minutes);
         Self { minutes: m }
     }
 
-    fn recalculate_time(minutes: i32) -> i32 {
-        let mut h = minutes / 60;
+    fn recalc_time(minutes: i32) -> i32 {
+        let h = minutes
+            .div_euclid(60)
+            .rem_euclid(24);
 
-        // adjust hours for rolling over minutes
-        if minutes < 0 && minutes % 60 != 0 {
-            h -= 1;
-        }
-
-        h %= 24;
-        if h < 0 {
-            h += 24;
-        }
-
-        let mut m = minutes;
-        m %= 60;
-        if m < 0 {
-            m += 60;
-        }
+        let m = minutes
+            .rem_euclid(60);
 
         h * 60 + m
     }
