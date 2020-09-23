7ea480b74d0acd18dd8c0c9b8b64eb779b743f14
Dennis O'Harlem <dennis.oharlem@gmail.com>
2020-09-23 03:34:42 -0400

    saddle ref

--- saddle-points/src/lib.rs
+++ saddle-points/src/lib.rs
@@ -1,40 +1,25 @@
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    // Algorithm:
-    // iterate through every number
-    // for every number:
-    // -- get max of a row
-    // -- get min of a column
-    // if true for both, add to output
 
-    // tests consider (row, column) location
-
+    // tests use (row, column) coords
     let mut res: Vec<(usize, usize)> = Vec::new();
 
-    let cols = &input[0];
-    let rows = &input;
+    let row_len = input.len();
 
-    for r in 0..rows.len() {
-        for c in 0..cols.len() {
+    for r in 0..row_len {
+        'inner: for c in 0..input[0].len() {
+            // validate row max
+            if &input[r][c] < input[r].iter().max().unwrap() {
+                continue;
+            }
 
-            // get row max
-            let mut row_max = u64::MIN;
-            for i in 0..cols.len() {
-                if input[r][i] > row_max {
-                    row_max = input[r][i];
+            // validate column min
+            for i in 0..row_len {
+                if &input[i][c] < &input[r][c] {
+                    break 'inner;
                 }
             }
 
-            // get column min
-            let mut col_min = u64::MAX;
-            for i in 0..rows.len() {
-                if input[i][c] < col_min {
-                    col_min = input[i][c];
-                }
-            }
-
-            if &input[r][c] >= &row_max && &input[r][c] <= &col_min {
-                res.push((r as usize, c as usize));
-            }
+            res.push((r as usize, c as usize));
         }
     }
 
