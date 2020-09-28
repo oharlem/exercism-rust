d9eabea32a9437379ba8a7a50c3d20b88d8be321
Dennis O'Harlem <dennis.oharlem@gmail.com>
2020-09-28 18:59:28 -0400

    anagram init

--- run-length-encoding/src/lib.rs
+++ run-length-encoding/src/lib.rs
@@ -1,5 +1,31 @@
 pub fn encode(source: &str) -> String {
-    unimplemented!("Return the run-length encoding of {}.", source);
+    // Example: "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB"  ->  "12WB12W3B24WB"
+    // Algorithm:
+    // - compare symbols with a previous one
+    // - keep a counter of repetitions
+    // - on symbols change,
+    // -- store the resulting number and a symbol in result
+    // -- if 1 repetition, skip 1 from result
+    // -- reset counter
+    let mut out = String::new();
+    let mut prev: char = '0';
+    let mut counter = 0;
+    for c in source.chars() {
+        if c != prev {
+            if counter == 1 {
+                out = format!("{}{}", out, prev);
+            } else {
+                out = format!("{}{}{}", out, counter, prev);
+            }
+            prev = c;
+            counter = 1;
+            continue;
+        }
+
+        counter += 1;
+    }
+
+    out
 }
 
 pub fn decode(source: &str) -> String {
